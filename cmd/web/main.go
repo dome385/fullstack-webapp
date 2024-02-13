6fe1132023c0c7a01942d68004a16a445ecdd3f1
dome385 <Niestroj@kaitech.local>
2024-02-13 13:30:39 +0100

    templates

---
+++ cmd/web/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"webapp/pkg/handlers"
+)
+
+const portNumber = ":8080"
+
+// main is the main application function
+func main() {
+
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
+
+	fmt.Printf(fmt.Sprintf("Starting application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
+}
